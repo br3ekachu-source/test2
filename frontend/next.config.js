4cbe5daee89ee8ae76159634d14538c8d77d6f3a
klushin.m <klushin.m@skdesign.ru>
2025-06-15 12:02:07 +0300

    upd

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,9 @@ const path = require('path');
 
 module.exports = {
   output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
       'Advert': path.resolve(__dirname, 'src/resources/Advert'),
       'SydnoComponents': path.resolve(__dirname, 'src/resources/SydnoComponents')
     };
